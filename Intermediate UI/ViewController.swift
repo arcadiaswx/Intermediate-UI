a57f15beeba6b5132db5ebf8e5e251f7ebdde17a
Craig Booker <info@arcadiasoftworks.com>
2019-01-24 16:40:25 -0600

    Added data to the pickerView and functions to load the data.

--- Intermediate UI/ViewController.swift
+++ Intermediate UI/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  
 
+  let data:[String] = ["Item 1","Item 2","Item 3", "Item 4","Item 5","Item 6"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
 
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+
+  }
+  
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return data.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return data[row]
+  }
+  
 }
 
